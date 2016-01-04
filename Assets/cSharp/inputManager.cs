e24fd8810ad21aed7dd10f08059f7183f3774ec4
zhangyi <zhangyi@firewinggames.com>
2016-01-04 19:15:35 +0800

    增加input manager

---
+++ Assets/cSharp/inputManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inputManager : MonoBehaviour {
+
+	public void Init ()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach(Touch touch in Input.touches)
+		{
+			Debug.Log(touch.fingerId);
+			Debug.Log(touch.phase);
+			Debug.Log(touch.deltaPosition);
+		}
+
+	}
+}
--- Assets/cSharp/start.cs
+++ Assets/cSharp/start.cs
@@ -9,17 +9,22 @@ public class start : MonoBehaviour {
 
 		Debug.Log("start Start");
 
+		// input
+		GameObject inputgo = new GameObject("inputManager");
+		inputManager inputmgr = inputgo.AddComponent<inputManager>();
+		inputmgr.Init();
+		GameObject.DontDestroyOnLoad(inputgo);
+
+		// ui
 		GameObject uigo = new GameObject("uiManager");
 		uiManager uimgr = uigo.AddComponent<uiManager>();
 		uimgr.Init();
-
 		GameObject.DontDestroyOnLoad(uigo);
 
-
+		// lua
 		GameObject go = new GameObject("luaManager");
 		luaManager luamgr = go.AddComponent<luaManager>();
 		luamgr.Init();
-
 		GameObject.DontDestroyOnLoad(go);
 	}
 	
